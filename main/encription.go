c96e383ec7e941c3b9e73726a9f8c3669a5c1e60
hm-Reik <richard.reik@hm.edu>
2021-06-25 08:22:58 +0200

    getCert request wirf fehler da kid angeblich nicht vorhanden

--- main/encription.go
+++ main/encription.go
@@ -1,120 +1,64 @@
 package main
 
 import (
-	"crypto/rand"
 	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"flag"
+	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 	"strings"
 
 	jose "gopkg.in/square/go-jose.v2"
 )
 
-const (
-	localCertFile = "ServerRootCert"
-)
+type dummyNonceSource struct{}
 
-// Quelle: https://pkg.go.dev/gopkg.in/square/go-jose.v2
-// Quelle: https://stackoverflow.com/questions/24455147/how-do-i-send-a-json-string-in-a-post-request-in-go
-// Quelle: https://forfuncsake.github.io/post/2017/08/trust-extra-ca-cert-in-go-app/
-// https://golangcode.com/download-a-file-from-a-url/
-// https://github.com/minio/minio-go/issues/1019
+func (n dummyNonceSource) Nonce() (string, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
 
-type Header struct {
-	// The algorithm used for signature.
-	Algorithm string `json:"alg"`
-
-	// Represents the token type.
-	Typ string `json:"typ"` //todo: <- was ist das?
-
-	// The optional hint of which key is being used.
-	KeyID string `json:"kid,omitempty"`
+	res, err := client.Head("https://localhost:14000/nonce-plz")
+	if err != nil {
+		panic(err)
+	}
+	ua := res.Header.Get("Replay-Nonce")
+	return ua, nil
 }
 
-func combine(nonce string) {
-	println("Encryption start")
-
-	// Get Root Certifikate
-	fileURL := "https://localhost:15000/roots/0"
-	error := DownloadFile("ServerRootCert", fileURL)
-	if error != nil {
-		panic(error)
-	}
-
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+func newAccount(privateKey *rsa.PrivateKey) (order_url string) {
+	var signerOpts = jose.SignerOptions{NonceSource: dummyNonceSource{}}
+	signerOpts.WithHeader("jwk", jose.JSONWebKey{Key: privateKey.Public()})
+	signerOpts.WithHeader("url", "https://localhost:14000/sign-me-up")
+	signer, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.RS256, Key: privateKey}, &signerOpts)
 	if err != nil {
 		panic(err)
 	}
-	println(privateKey)
-	// Instantiate a signer using RSASSA-PSS (SHA512) with the given private key.
-	signer, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.PS512, Key: privateKey}, nil)
-	value := jose.JSONWebKey{Key: privateKey.Public()}
-	testHeader := jose.Header{JSONWebKey: &value, Algorithm: string(jose.PS512), Nonce: nonce, ExtraHeaders: map[jose.HeaderKey]interface{}{"url": "https://localhost:14000/sign-me-up"}}
-	sig := []jose.Signature{jose.Signature{Protected: testHeader}} //evtl Signature?
-	jwsig := jose.JSONWebSignature{Signatures: sig}
+	println("test")
 
-	fmt.Printf("jwsig: %v\n", jwsig)
-
-	// Herr Schreck fragen ...
-
-	if err != nil {
-		panic(err)
-	}
-	println("Signer:%v", signer)
-
-	// Sign a sample payload. Calling the signer returns a protected JWS object,
-	// which can then be serialized for output afterwards. An error would
-	// indicate a problem in an underlying cryptographic primitive.
-	var payload = []byte("Lorem ipsum dolor sit amet")
+	var testor [1]string
+	testor[0] = "mailto:test.test@test.de"
+	payload := map[string]interface{}{"termsOfServiceAgreed": true, "contact": testor}
+	byts, _ := json.Marshal(payload)
+	fmt.Println(string(byts))
 	signer.Options()
-	object, err := signer.Sign(payload)
+	object, err := signer.Sign(byts)
 	if err != nil {
 		panic(err)
 	}
 
 	serialized := object.FullSerialize()
-	println("Payload: ", serialized)
-	// ----------------------------------------------------------------------------------------------------------------------------
 
-	insecure := flag.Bool("insecure-ssl", false, "Accept/Ignore all server SSL certificates")
-	flag.Parse()
-	rootCAs, _ := x509.SystemCertPool()
-	if rootCAs == nil {
-		rootCAs = x509.NewCertPool()
-	}
-	certs, err := ioutil.ReadFile(localCertFile)
-	if err != nil {
-		log.Fatalf("Failed to append %q to RootCAs: %v", localCertFile, err)
-	}
-	println(string(certs))
-
-	// Append our cert to the system pool
-	if ok := rootCAs.AppendCertsFromPEM(certs); !ok {
-		log.Println("No certs appended, using system certs only")
-	}
-
-	// Trust the augmented cert pool in our client
-	config := &tls.Config{
-		InsecureSkipVerify: *insecure,
-		RootCAs:            rootCAs,
-	}
-	tr := &http.Transport{TLSClientConfig: config}
+	tlsConfig := &tls.Config{}
+	tlsConfig.InsecureSkipVerify = true
+	tr := &http.Transport{TLSClientConfig: tlsConfig}
 	client := &http.Client{Transport: tr}
-	// ----------------------------------------------------------------------------------------------------------------------------
-	/* 	tlsConfig := &tls.Config{}
-	   	tlsConfig.InsecureSkipVerify = true
-	tr := &http.Transport{TLSClientConfig: config}
-	client := &http.Client{Transport: tr} */
 
 	req, err := http.NewRequest("POST", "https://localhost:14000/sign-me-up", strings.NewReader(serialized))
 	req.Header.Add("Content-Type", "application/jose+json")
+
 	resp, err := client.Do(req)
 	if err != nil {
 		println(err.Error())
@@ -122,54 +66,65 @@ func combine(nonce string) {
 	}
 	defer resp.Body.Close()
 	println("HTTP result status: ", resp.Status)
-	println("HTTP result body: ", resp.Body)
-
-	/* 	// Parse the serialized, protected JWS object. An error would indicate that
-	   	// the given input did not represent a valid message.
-	   	object, err = jose.ParseSigned(serialized)
-	   	if err != nil {
-	   		panic(err)
-	   	}
-	   	println("Object:%v", object)
-
-	   	// Now we can verify the signature on the payload. An error here would
-	   	// indicate that the message failed to verify, e.g. because the signature was
-	   	// broken or the message was tampered with.
-	   	output, err := object.Verify(&privateKey.PublicKey)
-	   	if err != nil {
-	   		panic(err)
-	   	}
-	   	println("Output:%v", string(output)) */
-	println("Encription finished")
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		println(err.Error())
+		panic(err)
+	}
+	println("HTTP result body: ", string(body))
+	m := make(map[string]json.RawMessage)
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		println(err.Error())
+		panic(err)
+	}
+	println("Account created")
+	return string(m["orders"])
 }
 
-func DownloadFile(filepath string, url string) error {
+type identifier struct {
+	key   string
+	value string
+}
+
+func newCertificate(privateKey *rsa.PrivateKey, order_url string) {
+	var signerOpts = jose.SignerOptions{NonceSource: dummyNonceSource{}}
+	println(order_url)
+	signerOpts.WithHeader("kid", order_url)
+	signerOpts.WithHeader("url", "https://localhost:14000/order-plz")
+	signer, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.RS256, Key: privateKey}, &signerOpts)
+	if err != nil {
+		panic(err)
+	}
+	testinator := map[string]string{"type": "dns", "value": "www.example.org"}
+	payload := map[string]interface{}{"identifiers": testinator, "notBefore": "2021-08-01T00:04:00+04:00", "notAfter": "2021-08-08T00:04:00+04:00"}
+	byts, _ := json.Marshal(payload)
+	fmt.Println(string(byts))
+	signer.Options()
+	object, err := signer.Sign(byts)
+	if err != nil {
+		panic(err)
+	}
+
+	serialized := object.FullSerialize()
+	println("Payload: ", serialized)
+
 	tlsConfig := &tls.Config{}
 	tlsConfig.InsecureSkipVerify = true
 	tr := &http.Transport{TLSClientConfig: tlsConfig}
 	client := &http.Client{Transport: tr}
 
-	// Get the data
-	test := ""
-	haus := strings.NewReader(test)
-	req, err := http.NewRequest("GET", url, haus)
+	req, err := http.NewRequest("POST", "https://localhost:14000/order-plz", strings.NewReader(serialized))
+	req.Header.Add("Content-Type", "application/jose+json")
+
 	resp, err := client.Do(req)
 	if err != nil {
-		if err.Error() != "Get \"https://localhost:15000/roots/0\": x509: certificate signed by unknown authority" {
-			return err
-		}
+		println(err.Error())
+		panic(err)
 	}
 	defer resp.Body.Close()
-	println("Get Root Certifikate HTTP status: ", resp.Status)
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
+	println("HTTP result status: ", resp.Status)
+	body, err := io.ReadAll(resp.Body)
+	println("HTTP result body: ", string(body))
 
-	println("Get Root Certifikate completed")
-	return err
 }
--- main/main.go
+++ main/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/tls"
-	"encoding/base64"
-	"encoding/json"
 	"net/http"
-	"os"
 )
 
 // Quelle: https://blog.golang.org/json
@@ -18,39 +17,25 @@ type Message struct {
 
 func main() {
 	println("start")
-	nonce := getNonce()
-	combine(nonce)
-
-	// Quelle: https://golang.org/pkg/encoding/base64/
-	// todo: prüfen was "alg" ist und ob es bleiben kann / jwk erstellen
-	m := Message{"ES256", "todo", nonce, "https://localhost:14000/sign-me-up"}
-
-	b, err := json.Marshal(m)
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		panic(err)
 	}
-	encoder := base64.NewEncoder(base64.StdEncoding, os.Stdout)
-	encoder.Write(b)
-	encoder.Close()
 
-	println()
-	println("ende")
+	order_url := newAccount(privateKey)
+	newCertificate(privateKey, order_url)
 }
 
 func getNonce() string {
-	// Quelle: https://stackoverflow.com/questions/12122159/how-to-do-a-https-request-with-bad-certificate
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{Transport: tr}
 
-	// Quelle: https://stackoverflow.com/questions/38563285/how-to-execute-a-head-request-in-go
 	res, err := client.Head("https://localhost:14000/nonce-plz")
 	if err != nil {
 		panic(err)
 	}
-	// Quelle: https://stackoverflow.com/questions/46021330/how-can-i-read-a-header-from-an-http-request-in-golang/46022272
 	ua := res.Header.Get("Replay-Nonce")
-	println("Replay-Nonce:%v", ua)
 	return ua
 }
