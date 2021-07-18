bce7c317677d8ff88e0fc519b285d916b33ec902
hm-Reik <richard.reik@hm.edu>
2021-07-18 18:48:16 +0200

    use open ssl to create CSR better error messages

--- main/keyPairGenerator.go
+++ main/keyPairGenerator.go
@@ -18,37 +18,37 @@ func readCSRFromFile() {
 	cmdHome := exec.Command("cd", "..")
 	err := cmdHome.Run()
 	if err != nil {
-		println("cd .. Befehl konnte nicht ausgeführt werden", err)
+		println("cd .. Befehl konnte nicht ausgeführt werden", err.Error())
 	}
 
 	cmdTpm2Tss := exec.Command("cd", "tpm2-tss-engine/")
 	err = cmdTpm2Tss.Run()
 	if err != nil {
-		println("cd tpm2-tss-engine/ Befehl konnte nicht ausgeführt werden", err)
+		println("cd tpm2-tss-engine/ Befehl konnte nicht ausgeführt werden", err.Error())
 	}
 
 	cmdGenkey := exec.Command("tpm2tss-genkey", "-a", "rsa", "scriptKey.tss")
 	err = cmdGenkey.Run()
 	if err != nil {
-		println("tpm2tss-genkey -a rsa scriptKey.tss Befehl konnte nicht ausgeführt werden", err)
+		println("tpm2tss-genkey -a rsa scriptKey.tss Befehl konnte nicht ausgeführt werden", err.Error())
 	}
 
 	cmdCreateCSR := exec.Command("openssl", "req", "-new", "-x509", "-engine", "tpm2tss", "-key", "scriptKey.tss", "-keyform engine", "-out", "scriptcsr.csr")
 	err = cmdCreateCSR.Run()
 	if err != nil {
-		println("openssl req -new -x509 -engine tpm2tss -key scriptKey.tss -keyform engine -out scriptcsr.csr Befehl konnte nicht ausgeführt werden", err)
+		println("openssl req -new -x509 -engine tpm2tss -key scriptKey.tss -keyform engine -out scriptcsr.csr Befehl konnte nicht ausgeführt werden", err.Error())
 	}
 
 	cmdCleanup := exec.Command("rm", "scriptKey.tss")
 	err = cmdCleanup.Run()
 	if err != nil {
-		println("rm scriptKey.tss Befehl konnte nicht ausgeführt werden", err)
+		println("rm scriptKey.tss Befehl konnte nicht ausgeführt werden", err.Error())
 	}
 
 	cmdMoveFile := exec.Command("mv", "scriptcsr.csr", "/home/pi/ACMEclinet")
 	err = cmdMoveFile.Run()
 	if err != nil {
-		println("mv scriptcsr.csr /home/pi Befehl konnte nicht ausgeführt werden", err)
+		println("mv scriptcsr.csr /home/pi Befehl konnte nicht ausgeführt werden", err.Error())
 	}
 
 	if _, err := os.Stat("scriptcsr.csr"); os.IsNotExist(err) {
