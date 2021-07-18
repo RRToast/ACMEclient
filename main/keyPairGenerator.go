1cd405a2e214c3bd47110919f6476b7f042ddaf0
hm-Reik <richard.reik@hm.edu>
2021-07-18 18:53:33 +0200

    use open ssl to create CSR, no cd required

--- main/keyPairGenerator.go
+++ main/keyPairGenerator.go
@@ -15,7 +15,7 @@ mv scriptcsr.csr /home/pi
 */
 
 func readCSRFromFile() {
-	cmdHome := exec.Command("cd", "..")
+	/* cmdHome := exec.Command("cd", "..")
 	err := cmdHome.Run()
 	if err != nil {
 		println("cd .. Befehl konnte nicht ausgeführt werden", err.Error())
@@ -25,27 +25,31 @@ func readCSRFromFile() {
 	err = cmdTpm2Tss.Run()
 	if err != nil {
 		println("cd tpm2-tss-engine/ Befehl konnte nicht ausgeführt werden", err.Error())
-	}
+	} */
 
 	cmdGenkey := exec.Command("tpm2tss-genkey", "-a", "rsa", "scriptKey.tss")
-	err = cmdGenkey.Run()
+	cmdGenkey.Dir = "home/pi/tpm2-tss-engine/"
+	err := cmdGenkey.Run()
 	if err != nil {
 		println("tpm2tss-genkey -a rsa scriptKey.tss Befehl konnte nicht ausgeführt werden", err.Error())
 	}
 
 	cmdCreateCSR := exec.Command("openssl", "req", "-new", "-x509", "-engine", "tpm2tss", "-key", "scriptKey.tss", "-keyform engine", "-out", "scriptcsr.csr")
+	cmdCreateCSR.Dir = "home/pi/tpm2-tss-engine/"
 	err = cmdCreateCSR.Run()
 	if err != nil {
 		println("openssl req -new -x509 -engine tpm2tss -key scriptKey.tss -keyform engine -out scriptcsr.csr Befehl konnte nicht ausgeführt werden", err.Error())
 	}
 
 	cmdCleanup := exec.Command("rm", "scriptKey.tss")
+	cmdCleanup.Dir = "home/pi/tpm2-tss-engine/"
 	err = cmdCleanup.Run()
 	if err != nil {
 		println("rm scriptKey.tss Befehl konnte nicht ausgeführt werden", err.Error())
 	}
 
 	cmdMoveFile := exec.Command("mv", "scriptcsr.csr", "/home/pi/ACMEclinet")
+	cmdMoveFile.Dir = "home/pi/tpm2-tss-engine/"
 	err = cmdMoveFile.Run()
 	if err != nil {
 		println("mv scriptcsr.csr /home/pi Befehl konnte nicht ausgeführt werden", err.Error())
